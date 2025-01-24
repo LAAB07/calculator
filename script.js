a9b8d34eb874a7284b6a390c919f07bd4c3d6b50
LAAB <111896274+LAAB07@users.noreply.github.com>
2025-01-24 11:54:19 -0700

    Create a basic HTML calculator.

--- script.js
+++ script.js
@@ -1,45 +1,48 @@
 let firstNumber = 0;
 let secondNumber = 0;
-// let operator = "";
+let operator = "";
 
 function add(a,b){
-
     firstNumber = a;
     secondNumber = b;
-
     return firstNumber+secondNumber;
-
 }
 
 function subtract(a,b){
-
     firstNumber = a;
     secondNumber = b;
-
     return firstNumber-secondNumber;
-
 }
 
 function multiply(a,b){
-
     firstNumber = a;
     secondNumber = b;
-
     return firstNumber*secondNumber;
-
 }
 
 function divide(a,b){
-
     firstNumber = a;
     secondNumber = b;
-
     return firstNumber/secondNumber;
-
 }
 
-console.log(add(5,7));
-console.log(subtract(10, 5));
-console.log(multiply(8,8));
-console.log(divide(45,9));
+function operate(a,b,op){
+    operator = op;
+    if(operator==="+"){
+        return add(a,b);
+    } else if(operator==="-"){
+        return subtract(a,b);
+    } else if(operator==="*"){
+        return multiply(a,b);
+    } else if(operator==="/"){
+        return divide(a,b);
+    }
+}
+
+// console.log(add(5,7));
+// console.log(subtract(10, 5));
+// console.log(multiply(8,8));
+// console.log(divide(45,9));
+
+console.log(operate(5,5,"/"));
 
