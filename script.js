58a7caf9af5267d70b6a66b8b04e1ee92223c507
LAAB <111896274+LAAB07@users.noreply.github.com>
2025-01-24 10:53:36 -0700

    Create basic math operations and show them in console.

---
+++ script.js
@@ -0,0 +1,45 @@
+let firstNumber = 0;
+let secondNumber = 0;
+// let operator = "";
+
+function add(a,b){
+
+    firstNumber = a;
+    secondNumber = b;
+
+    return firstNumber+secondNumber;
+
+}
+
+function subtract(a,b){
+
+    firstNumber = a;
+    secondNumber = b;
+
+    return firstNumber-secondNumber;
+
+}
+
+function multiply(a,b){
+
+    firstNumber = a;
+    secondNumber = b;
+
+    return firstNumber*secondNumber;
+
+}
+
+function divide(a,b){
+
+    firstNumber = a;
+    secondNumber = b;
+
+    return firstNumber/secondNumber;
+
+}
+
+console.log(add(5,7));
+console.log(subtract(10, 5));
+console.log(multiply(8,8));
+console.log(divide(45,9));
+
