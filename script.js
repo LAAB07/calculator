e221b5f6e93bbba99a71781bc8c2a237d4b62b8d
LAAB <111896274+LAAB07@users.noreply.github.com>
2025-01-28 13:26:56 -0700

    Make the calculator work by clicking the HTML buttons and show the result n the display.

--- script.js
+++ script.js
@@ -1,72 +1,111 @@
+//Variables declaration
+
+let a = "";
+let b = "";
+let op = "";
 let firstNumber = "";
-let secondNumber = 0;
+let secondNumber = "";
 let operator = "";
+let result = "";
+let switchNumber = false;
+
+//Getting HTML elements
 
 let display = document.getElementById("top-calc");
-
-function add(a,b){
-    firstNumber = a;
-    secondNumber = b;
-    return firstNumber+secondNumber;
-}
-
-function subtract(a,b){
-    firstNumber = a;
-    secondNumber = b;
-    return firstNumber-secondNumber;
-}
-
-function multiply(a,b){
-    firstNumber = a;
-    secondNumber = b;
-    return firstNumber*secondNumber;
-}
-
-function divide(a,b){
-    firstNumber = a;
-    secondNumber = b;
-    return firstNumber/secondNumber;
-}
-
-function operate(a,b,op){
-    operator = op;
-    if(operator==="+"){
-        return add(a,b);
-    } else if(operator==="-"){
-        return subtract(a,b);
-    } else if(operator==="*"){
-        return multiply(a,b);
-    } else if(operator==="/"){
-        return divide(a,b);
-    }
-}
-
-// console.log(add(5,7));
-// console.log(subtract(10, 5));
-// console.log(multiply(8,8));
-// console.log(divide(45,9));
-
-// console.log(operate(5,5,"/"));
-
 const numberButton = document.querySelectorAll(".number");
 const clearButton = document.getElementById("clear");
+const equalsButton = document.getElementById("equals");
+const operatorButton = document.querySelectorAll(".operator");
+
+//Click events
 
 numberButton.forEach(button =>{
     button.addEventListener("click", setDisplay);
 });
 
+operatorButton.forEach(button =>{
+    button.addEventListener("click", setOperator);
+})
+
 clearButton.addEventListener("click", clearDisplay);
 
+equalsButton.addEventListener("click", operate);
+
+//Functions
+
 function setDisplay(){
-    if(firstNumber.length<=8){
-    firstNumber += this.textContent;
-    display.textContent = firstNumber;
-    console.log(firstNumber);
+    if(switchNumber === false){
+        if(a.length<=8){
+        a += this.textContent;
+        display.textContent = a;
+        console.log("The first number is: "+ a);
+        console.log(switchNumber);
+        firstNumber = Number(a);
+        }
+    } else {
+        if(b.length<=8){
+            b += this.textContent;
+            display.textContent = b;
+            console.log("The second number is: "+ b);
+            console.log(switchNumber);
+            secondNumber = Number(b);
+        }
     }
+
 }
 
 function clearDisplay(){
+    a = "";
+    b = "";
+    op = "";
     firstNumber = "";
+    secondNumber = "";
+    operator = "";
     display.textContent = "0";
+    switchNumber = false;
+    operatorButton.style.color = "white";
+    console.clear();
+}
+
+function setOperator(){
+    op = this.textContent;
+    console.log(op);
+    switchNumber = true;
+    console.log(switchNumber);
+    operator = op;
+    this.style.color = "yellow";
+}
+
+function operate(a,b,op){
+    console.log("Im listening");
+    console.log("first number: " + firstNumber);
+    console.log("second number: " + secondNumber);
+    console.log("operator is: " + operator);
+    if(operator==="+"){
+        result = add(firstNumber,secondNumber);
+    } else if(operator==="-"){
+        result = subtract(firstNumber,secondNumber);
+    } else if(operator==="*"){
+        result = multiply(firstNumber,secondNumber);
+    } else if(operator==="/"){
+        result = divide(firstNumber,secondNumber);
+    }
+    display.textContent = result;
+}
+
+function add(a,b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
 }
 
